26e949940c426c4cb0299c05c56642165e0e628b
Dangkeys <TajdangZaa@gmail.com>
2023-06-19 09:51:30 +0700

    test git

---
+++ Hewkao.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int num;
+int main()
+{
+    scanf("%d", &num);
+
+    if(num < 99 && num > 1)
+    {
+        for(int i = 1; i <= num; i++)
+        {
+            char answer[] = "*" * i;
+            printf("%s", )
+        }
+    } else {
+        printf("ERROR");
+    }
+}
